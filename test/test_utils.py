f69a06b160c22d63ba41496e7c50f8852f27ea27
Eeo Jun <packwolf58@gmail.com>
2016-03-30 14:11:25 +0800

    DRYer test code

--- test/test_utils.py
+++ test/test_utils.py
@@ -1,5 +1,4 @@
-import takeit.utils
-from itertools import chain
+import takeit.utils as utils
 
 
 def test_get_urls():
@@ -11,7 +10,7 @@ def test_get_urls():
     </body>
     </html>
     '''
-    assert list(takeit.utils.get_urls(html)) == ['one', 'two']
+    assert list(utils.get_urls(html)) == ['one', 'two']
 
 
 def test_filename_and_url():
@@ -19,7 +18,7 @@ def test_filename_and_url():
         'https://abc.net/url.js',
         'https://abc.net/two.js',
     ]
-    given = dict(takeit.utils.filename_and_url(urls))
+    given = dict(utils.filename_and_url(urls))
     expected = {
         'url.js': urls[0],
         'two.js': urls[1],
@@ -28,7 +27,7 @@ def test_filename_and_url():
 
 
 def test_parse_options():
-    given = takeit.utils.parse_options('''
+    given = utils.parse_options('''
     # not included
     # not included too
     one!
@@ -40,8 +39,8 @@ def test_parse_options():
 
 def test_generate_editor_contents():
     files = ['abc', 'def']
-    given = takeit.utils.generate_editor_contents(files)
-    assert given == '\n'.join([takeit.utils.MESSAGE] + files)
+    given = utils.generate_editor_contents(files)
+    assert given == '\n'.join([utils.MESSAGE] + files)
 
 
 def test_flatten():
@@ -49,6 +48,6 @@ def test_flatten():
     g = [(3, 3), (4, 4)]
 
     indexes = list([f, g])
-    index = takeit.utils.flatten(indexes)
+    index = utils.flatten(indexes)
 
     assert list(index) == f + g
